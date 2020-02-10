01bdca6a0d9ca2deb752f1f2f68fd4a1a3815da3
MarcelloVSilva <marcellovcs@gmail.com>
2020-02-10 01:09:17 -0300

    new funcs, enhance main sh, remove sync sh from git

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,9 +1,10 @@
 set -e
 
 REPO_URL=$1
+FILE_OUTPUT=$2
 
 git clone $REPO_URL
 
-echo $REPO_URL | grep -o "/.*.git"
+findead $REPO_URL > $FILE_OUTPUT
 
-rm -rf vocequisdizerlustre
+rm -rf $REPO_URL
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 const fetch = require("node-fetch");
+const fs = require('fs');
 
 app.use(express.static('public'))
 
 app.get('/url', async function (req, res) {
   res.writeHead(200, { 'Content-type': 'text/plain' });
   try {
-    const isReactRepositorieRes = await isReactRepositorie(req.query.repoUrl);
-    res.write(isReactRepositorieRes);
+    let { repoUrl } = req.query;
+    repoUrl = repoUrl.indexOf('.git') > -1 ? repoUrl.replace('.git', '') : repoUrl;
+    const [user, repoName] = getUserAndRepoNamesFromUrl(repoUrl);
+    await isReactRepositorie(repoUrl);
+    const needNewFindeadCheck = await needNewCheck(repoUrl, user, repoName);
+    if (needNewFindeadCheck) {
+      res.write('run findead')
+    } else res.write('lastResultFile')
   } catch (error) {
     res.write(error);
+    res.end();
   }
   res.end();
 });
@@ -18,14 +26,12 @@ app.get('/url', async function (req, res) {
 function getUserAndRepoNamesFromUrl(repoUrl) {
   const regex = new RegExp('(.com\/)(.*)\/(.*)');
   let res = repoUrl.match(regex);
-  console.log(res)
-
+  return [res[2], res[3]];
 }
 
 function isReactRepositorie(repoUrl) {
   return new Promise((resolve, reject) => {
-    let repoUrlRaw = repoUrl.indexOf('.git') > -1 ? repoUrl.replace('.git', '') : repoUrl;
-    repoUrlRaw = repoUrlRaw.replace('github', 'raw.githubusercontent');
+    let repoUrlRaw = repoUrl.replace('github', 'raw.githubusercontent');
     repoUrlRaw = `${repoUrlRaw}/master/package.json`
     fetch(repoUrlRaw, { method: 'get' })
       .then(res => res.json())
@@ -36,7 +42,7 @@ function isReactRepositorie(repoUrl) {
         Array.isArray(isReactRepo) ? resolve(`${repoUrl} is a react repositorie`) : reject(`${repoUrl} is not a react repositorie`)
       })
       .catch(err => {
-        const { type } = err;
+        const { type, message } = err;
         switch (type) {
           case 'invalid-json':
             reject("Invalid or inexistent JSON.")
@@ -45,6 +51,37 @@ function isReactRepositorie(repoUrl) {
             reject(message)
             break;
         }
+      })
+  })
+}
+
+async function needNewCheck(repoUrl, user, repoName) {
+  const cacheFileLastModification = getCacheFileLastModification(user, repoName);
+  const repoLastModification = await getRepoLastModification(repoUrl);
+  return new Date(repoLastModification) > new Date(cacheFileLastModification);
+}
+
+function getCacheFileLastModification(user, repoName) {
+  try {
+    const stats = fs.statSync(`../cache_results/${user}.${repoName}.txt`)
+    return stats.mtime;
+  } catch (error) {
+    return 0;
+  }
+}
+
+function getRepoLastModification(repoUrl) {
+  return new Promise((resolve, reject) => {
+    let repoUrlApi = repoUrl.replace('github.com', 'api.github.com/repos');
+    repoUrlApi = `${repoUrlApi}/commits/master`;
+    fetch(repoUrlApi, { method: 'get' })
+      .then(res => res.json())
+      .then(data => {
+        const { date } = data.commit.author;
+        resolve(date);
+      })
+      .catch(err => {
+        reject(err.message)
       })
   })
 }
